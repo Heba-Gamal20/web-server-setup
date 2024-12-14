b831d6efb8634b9f7af5dcba9e9fbae9011ab80c
root <root@localhost.localdomain>
2024-12-14 16:53:41 -0500

    Add . README.md

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
+
 <?php echo "Hello World!"; ?>
+
+
+
+
+
 <?php
 $servername = "localhost";
 $username = "web_user";
@@ -13,7 +19,7 @@ if ($conn->connect_error) {
 $visitor_ip = $_SERVER['REMOTE_ADDR'];
 $current_time = date("Y-m-d H:i:s");
 
-echo "Hello, visitor from IP: $visitor_ip<br>";
+echo " visitor from IP: $visitor_ip<br>";
 echo "Current server time: $current_time";
 
 $conn->close();
